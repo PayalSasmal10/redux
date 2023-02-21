ab486065a635be49cb107b8106e6f03a6ee4e909
Payal Sasmal <sasmalpayal@gmail.com>
2023-02-21 12:48:28 +0530

    redux counter project multiple spaces of states management is done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,18 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector(state => state.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   const incrementHandler = () => {
     dispatch({type: 'increment'});
+  };
+
+  const increaseHandler = () => {
+    dispatch({type: 'increase', amount: 5});
   };
 
   const decrementHandler = () => {
@@ -17,9 +25,10 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
